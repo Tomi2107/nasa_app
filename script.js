cfd8ece988778c95a146a5072d25cc1d523b4bae
Tomi07 <tom.palmeyro@gmail.com>
2024-06-25 20:45:24 -0300

    proyecto en react vite con UTN e incluyeme

---
+++ script.js
@@ -0,0 +1,20 @@
+let req = new XMLHttpRequest();
+// let url = "https://api.nasa.gov/planetary/apod?api_key=";
+let url = "https://api.nasa.gov/planetary/apod?api_key=";
+let api_key = "rh6DGbKDmQCCYFd88OBsWh4OFIa5RwMnToN33gcX";
+
+// https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key="
+
+req.open("GET", url + api_key);
+req.send();
+
+req.addEventListener("load", function(){
+	if(req.status == 200 && req.readyState == 4){
+  	let response = JSON.parse(req.responseText);
+    document.getElementById("title").textContent = response.title;
+    document.getElementById("date").textContent = response.date;
+    document.getElementById("pic").src = response.hdurl;
+    document.getElementById("explanation").textContent = response.explanation
+    console.log(response);
+  }
+})
